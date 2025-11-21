118f350ff05b592dd3f038700e6526a97fb95703
박단현 <166380533+sodanhyun@users.noreply.github.com>
2025-11-21 16:22:32 +0900

    Initial Commit

---
+++ src/test/java/com/codehows/taelimbe/TaelimBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehows.taelimbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaelimBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
