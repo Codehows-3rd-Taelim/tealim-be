10fa9c19bb6344072b65a47d9ad38f85777eb86f
JYB0535 <jnk8528@gmail.com>
2025-11-26 11:27:50 +0900

    ㅇ
    ㅇ

---
+++ src/main/java/com/codehows/taelimbe/dto/LoginResponseDTO.java
@@ -0,0 +1,15 @@
+package com.codehows.taelimbe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class LoginResponseDTO
+{
+    private String jwtToken;
+
+    private boolean isAdmin;
+}
