e5163e7efe115f5384318322d1d4e1f40b9a8967
JYB0535 <jnk8528@gmail.com>
2025-11-28 09:25:16 +0900

    필요없는 파일 삭제

--- src/main/java/com/codehows/taelimbe/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.codehows.taelimbe.repository;
-//
-//import com.codehows.taelimbe.entity.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<User, Long> {
-//}
